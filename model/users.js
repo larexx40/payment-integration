336961323890b475b23398930eab1a69d858efb2
larexx40 <larexx40@gmail.com>
2023-01-09 05:53:21 +0100

    setting the user model relations

--- model/users.js
+++ model/users.js
@@ -43,6 +43,7 @@ class User extends Model {
 
       properties: {
         id: { type: 'integer' },
+        parentId: { type: ['integer', 'null'] },
         firstName: { type: 'string', minLength: 1, maxLength: 255 },
         lastName: { type: 'string', minLength: 1, maxLength: 255 },
         age: { type: 'number' },
@@ -70,55 +71,16 @@ class User extends Model {
     // Importing models here is one way to avoid require loops.
     const Transaction = require('./transactions');
 
+
     return {
-      pets: {
+      transaction: {
         relation: Model.HasManyRelation,
-        // The related model. This can be either a Model
-        // subclass constructor or an absolute file path
-        // to a module that exports one. We use a model
-        // subclass constructor `Animal` here.
-        modelClass: Animal,
+        modelClass: Transaction,
         join: {
-          from: 'persons.id',
-          to: 'animals.ownerId'
+          from: 'users.id',
+          to: 'transactions.tnx_id'
         }
       },
-
-      movies: {
-        relation: Model.ManyToManyRelation,
-        modelClass: Movie,
-        join: {
-          from: 'persons.id',
-          // ManyToMany relation needs the `through` object
-          // to describe the join table.
-          through: {
-            // If you have a model class for the join table
-            // you need to specify it like this:
-            // modelClass: PersonMovie,
-            from: 'persons_movies.personId',
-            to: 'persons_movies.movieId'
-          },
-          to: 'movies.id'
-        }
-      },
-
-      children: {
-        relation: Model.HasManyRelation,
-        modelClass: Person,
-        join: {
-          from: 'persons.id',
-          to: 'persons.parentId'
-        }
-      },
-
-      parent: {
-        relation: Model.BelongsToOneRelation,
-        modelClass: Person,
-        join: {
-          from: 'persons.parentId',
-          to: 'persons.id'
-        }
-      }
     };
   }
 }
