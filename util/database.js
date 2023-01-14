ce5e5401158423521fd0da4ff2fbabdf29e80b3b
larexx40 <larexx40@gmail.com>
2023-01-14 18:01:55 +0100

    database connection

--- util/database.js
+++ util/database.js
@@ -1,17 +1,25 @@
 const { Model } = require('objection');
 const Knex = require('knex');
-require("dotenv").require()
+require("dotenv").config()
 
 // Initialize knex.
 const knex = Knex({
     client: "mysql",
     connection: {
-      host: process.env.MYSQL_HOST,
-      user: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DATABASE,
+      // host: process.env.MYSQL_HOST,
+      // user: process.env.MYSQL_USER,
+      // password: process.env.MYSQL_PASSWORD,
+      // database: process.env.MYSQL_DATABASE,
+      host: 'localhost',
+      user: 'root',
+      password: '',	
+      database: 'test'
     },
   });
+
+  knex.raw("SELECT VERSION()").then(()=>{
+    console.log("Connected to database successfully");
+  })
   
   // Give the knex instance to objection.
   Model.knex(knex);
